f23d24332755a5aab00052b899660db2fab80719
payapula <bharathikannank05@gmail.com>
2022-11-01 20:26:32 +0530

    - Ex 4 Completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -42,7 +42,7 @@ const Header = () => {
             <Icon id="search" />
             <VisuallyHidden>Search</VisuallyHidden>
           </UnstyledButton>
-          <UnstyledButton>
+          <UnstyledButton onClick={() => setShowMobileMenu(true)}>
             <Icon id="menu" />
             <VisuallyHidden>Menu</VisuallyHidden>
           </UnstyledButton>
